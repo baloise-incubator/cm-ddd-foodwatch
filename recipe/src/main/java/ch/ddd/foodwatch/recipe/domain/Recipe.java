b410bb1fcc17e39da2fe94806462086121ce5360
Alain Latscha <alain.latscha@baloise.ch>
2020-07-09 13:53:33 +0200

    Recipe implemented

--- recipe/src/main/java/ch/ddd/foodwatch/recipe/domain/Recipe.java
+++ recipe/src/main/java/ch/ddd/foodwatch/recipe/domain/Recipe.java
@@ -1,4 +1,16 @@
 package ch.ddd.foodwatch.recipe.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+@Builder(toBuilder = true)
 public class Recipe {
+    private final Integer servings;
+    private final List<Ingredient> foodList;
+    private final List<String> steps;
 }
