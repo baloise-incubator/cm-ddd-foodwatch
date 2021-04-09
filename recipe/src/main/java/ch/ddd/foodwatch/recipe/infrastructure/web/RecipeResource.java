1dda72340b887d010422551d8a4e82a20417022f
Peter Kullmann <peter.kullmann@baloise.ch>
2021-04-09 14:07:26 +0200

    Recipe Resource/Repository returns only known recipes

--- recipe/src/main/java/ch/ddd/foodwatch/recipe/infrastructure/web/RecipeResource.java
+++ recipe/src/main/java/ch/ddd/foodwatch/recipe/infrastructure/web/RecipeResource.java
@@ -11,9 +11,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
+
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @RestController
 @RequestMapping(value = "/recipes", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -35,7 +39,8 @@ public class RecipeResource {
 
     @GetMapping(path = "/{recipeId}")
     public RecipeDto getRecipeById(@PathVariable("recipeId") int recipeId) {
-        return map(recipeRepository.findRecipeById(new RecipeId(recipeId)));
+        Optional<Recipe> maybeRecipe = recipeRepository.findRecipeById(new RecipeId(recipeId));
+        return map(maybeRecipe.orElseThrow(() -> new ResponseStatusException(NOT_FOUND, "Recipe does not exist")));
     }
 
     private RecipeDto map(Recipe recipe) {
